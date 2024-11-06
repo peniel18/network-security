7ef1c1b4b73433cfdc0b5edaa783a4e09ef4ca42
peniel18 <penieletornam18@gmail.com>
2024-11-06 20:57:11 +0000

    fix: dir paths issues

--- src/networksecurity/constant/training_pipeline/__init__.py
+++ src/networksecurity/constant/training_pipeline/__init__.py
@@ -20,6 +20,7 @@ Data Ingestion related constants
 """
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "peniel18"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2 
