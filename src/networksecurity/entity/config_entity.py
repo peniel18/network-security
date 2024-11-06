ec8683989648b97dae36bf8dbae72d3e56e20bb4
peniel18 <penieletornam18@gmail.com>
2024-11-06 20:48:42 +0000

    fix: wrong dir paths

--- src/networksecurity/entity/config_entity.py
+++ src/networksecurity/entity/config_entity.py
@@ -15,10 +15,10 @@ class TrainingPipelineConfig:
 class DataIngestionConfig:
     def __init__(self, training_pipeline_config: TrainingPipelineConfig):
         self.data_ingestion_dir: str = os.path.join(
-                training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR
+                training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME
             )
         self.feature_store_file_path: str = os.path.join(
-                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR
+                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
             )
         self.training_file_path = os.path.join(
                 self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME
