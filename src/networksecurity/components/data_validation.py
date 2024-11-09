426a90cbd6140c881cd273bf5a91a4431cc64dd5
peniel18 <penieletornam18@gmail.com>
2024-11-09 01:01:48 +0000

    Data Validation

--- src/networksecurity/components/data_validation.py
+++ src/networksecurity/components/data_validation.py
@@ -4,7 +4,7 @@ from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException 
 from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
 from networksecurity.utils.main_utils.utils import read_yaml_file
-from scipy.stats import k2_2samp
+from scipy.stats import ks_2samp
 import pandas as pd 
 import os 
 import sys
@@ -21,3 +21,71 @@ class DataValidation:
         except Exception as e: 
             logging.error("Error Ocurred in data_validation.py")
             raise NetworkSecurityException(e, sys)
+        
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame: 
+        try: 
+            return pd.read_csv(file_path)
+        except Exception as e: 
+            raise NetworkSecurityException(e, sys)
+
+    def validate_number_of_columns(self, dataframe: pd.DataFrame) -> bool: 
+        try: 
+            #logging.info("validate_number_of_columns as started ")
+            num_of_columns = len(self._schema_config)
+            logging.info(f"Required number of columns: {num_of_columns}")
+            logging.info(f"DataFrame columns: {len(dataframe.columns)}") 
+            # check 
+            if len(dataframe.columns) == num_of_columns: 
+                return True
+            else: 
+                return False 
+        except Exception as e: 
+            logging.error("Erorr occured in validate_number_of_columns as started ")
+            raise NetworkSecurityException(e, sys)
+
+    def validateNumericalColumns(self, dataframe: pd.DataFrame) -> bool: 
+        schemeFile = read_yaml_file(SCHEMA_FILE_PATH)
+        numericalColumns = schemeFile["numerical_columns"]
+        # get dtypes and name of columns
+        colNames = []
+        colTypes = []
+        for col in numericalColumns: 
+            for name, dataType in col.items(): 
+                colNames.append(name)
+                colTypes.append(dataType)
+        
+        for column in dataframe.columns: 
+            pass 
+
+        
+        #print(numericalColumns)
+
+    def initiate_data_validation(self) ->  DataIngestionArtifact: 
+        try: 
+            train_file_path = self.data_ingestion_artifact.trained_file_path
+            test_file_path = self.data_ingestion_artifact.test_file_path
+            # read teh data from train and test 
+            train_dataframe = DataValidation.read_data(train_file_path)
+            test_dataframe = DataValidation.read_data(test_file_path)
+            # validata number of columns 
+            TrainStatus = self.validate_number_of_columns(dataframe=train_dataframe)
+            TestStatus = self.validate_number_of_columns(dataframe=test_dataframe)
+            if not TrainStatus: 
+                error_message = f"Train DataFrame does not contain the required number of columns"
+                logging.info(error_message)
+
+            if not TestStatus: 
+                error_message = f"Test DataFrame does not contain the required number of columns"
+                logging.info(error_message)
+
+
+        except Exception as e: 
+            logging.error("Error occured in intiate_data_validation")
+            raise NetworkSecurityException(e, sys)
+        
+
+    
+if __name__ == "__main__":
+    dataV = DataValidation(DataIngestionArtifact)
+    dataV.validateNumericalColumns()
--- src/networksecurity/utils/main_utils/utils.py
+++ src/networksecurity/utils/main_utils/utils.py
@@ -12,7 +12,7 @@ import pickle
 def read_yaml_file(file_path: str) -> dict: 
     logging.info("function: read_yaml_file has started")
     try: 
-        with open(file=file_path, "rb") as yaml_file: 
+        with open(file_path, "rb") as yaml_file: 
             return yaml.safe_load(yaml_file)
     except Exception as e: 
         logging.error("Error Ocurred in function read_yaml_file in utils")
