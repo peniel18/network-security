323a3d9a669b4fa0604fe1bf97b8f75bde03c72c
peniel18 <penieletornam18@gmail.com>
2024-11-12 15:59:58 +0000

    data Transformation updated

--- src/networksecurity/components/data_transformation.py
+++ src/networksecurity/components/data_transformation.py
@@ -13,3 +13,40 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging 
 from networksecurity.utils.main_utils.utils import save_numpy_array_data, save_object
 
+
+class DataTransformation:
+    def __init__(self,
+     data_validation_artifact: DataValidationArtifact,
+     data_transformation_config: DataTransformationConfig
+     ):
+        try: 
+            self.data_validation_artifact = data_validation_artifact
+            self.data_transformation_config = data_transformation_config
+        except Exception as e: 
+            raise NetworkSecurityException(e, sys)
+
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame: 
+        try: 
+            return pd.read_csv(file_path)
+        except Exception as e: 
+            raise NetworkSecurityException(e, sys) 
+
+
+    def InitiateDataTransformation(self) -> DataTransformationArtifact:
+        logging.info("Data Transformation has Started")
+        try: 
+            train_df = DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_df = DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
+            # drop target column 
+            train_features = train_df.drop(columns=[TARGET_COLUMN], axis="columns")
+            train_target_feature = train_df[TARGET_COLUMN]
+            train_target = train_target_feature.replace(-1, 0)
+            
+            test_features = test_df.drop(columns=[TARGET_COLUMN], axis="columns")
+            test_target_feature = test_df[TARGET_COLUMN]
+            test_target = test_target_feature.replace(-1, 0)
+
+            
+        except Exception as e: 
+            raise NetworkSecurityException(e, sys)
